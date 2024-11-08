a170cfb3ce3ca159fba7cb0a5ec4a3627e03a4cf
rafa <rmarmor406@g.eeducaand.es>
2024-11-08 12:50:19 +0100

    6

---
+++ ordenarString/src/ordenarstring/OrdenarString.java
@@ -0,0 +1,13 @@
+
+package ordenarstring;
+import java.util.*;
+public class OrdenarString {
+
+    public static void main(String[] args) {
+        String cadena[]={"hola","Soy","soy","rafa"};
+        Arrays.sort(cadena);
+        System.out.println(Arrays.toString(cadena));
+        
+    }
+    
+}
