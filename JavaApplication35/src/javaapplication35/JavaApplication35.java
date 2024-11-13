83b1410fc6b268cac3a8e6500063202e05cb2f01
rafa <rmarmor406@g.eeducaand.es>
2024-11-13 09:25:23 +0100

    12

---
+++ JavaApplication35/src/javaapplication35/JavaApplication35.java
@@ -0,0 +1,18 @@
+package javaapplication35;
+import java.util.*;
+/**
+ *
+ * @author usuario
+ */
+public class JavaApplication35 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int v[]={4,9,15,16,20};
+        int pos=Arrays.binarySearch(v,23);
+        System.out.println(pos);
+    }
+    
+}
