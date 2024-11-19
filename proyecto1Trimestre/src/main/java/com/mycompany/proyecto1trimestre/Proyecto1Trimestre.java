61242b42ea3b130c88664d78a81063023e954a37
rafa <rmarmor406@g.eeducaand.es>
2024-11-19 14:41:31 +0100

    241119

--- proyecto1Trimestre/src/main/java/com/mycompany/proyecto1trimestre/Proyecto1Trimestre.java
+++ proyecto1Trimestre/src/main/java/com/mycompany/proyecto1trimestre/Proyecto1Trimestre.java
@@ -1,16 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyecto1trimestre;
+import java.util.*;
 
-/**
- *
- * @author usuario
- */
 public class Proyecto1Trimestre {
-
+    
+    public static void menu(){
+        System.out.println("MEMORIÓN");
+        
+        System.out.println("Pulsa 'a' para entrar a los ajustes");
+        
+        System.out.println("Pulsa 'e' para empezar el juego");
+        
+        System.out.println("Pulsa 's' para salir");
+    }
+    
+    public static char[][] generarMatriz(int fila, int columna){
+      char matriz[][]=new char[fila][columna];
+      char simbolos[]=new char[0];
+      int pos=0;
+      for(int i=33;i<127;i++){
+          simbolos=Arrays.copyOf(simbolos,simbolos.length+1);
+          simbolos[pos]=(char)i;
+          System.out.println(simbolos);
+      }
+      for(int i=0;i<fila;i++){
+          for(int j=0;j<columna;j++){
+              matriz[i][j]=(char)(Math.random()*(127-33)+33);
+          }
+      }
+      return matriz;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean salir=false;
+        char opcion;
+        String matriz;
+        Scanner leerC=new Scanner(System.in);
+        Scanner leerN=new Scanner(System.in);
+        
+        while(salir==false){
+            menu();
+            opcion=leerC.next().charAt(0);
+            switch(opcion){
+                
+                case 'a': 
+                    
+                break;
+                
+                case 'e': 
+                    matriz=Arrays.toString(generarMatriz(3,4));
+                    System.out.println(matriz);
+                break;
+                
+                case 's': salir=true;
+                break;
+            }
+        
+        
+        
+        }
     }
 }
