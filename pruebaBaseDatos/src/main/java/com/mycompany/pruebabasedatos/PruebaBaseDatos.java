a3ac9c1c0a9b451058e10b11f8b49411ca9f7abc
rafa <rmarmor406@g.eeducaand.es>
2025-03-25 14:31:10 +0100

    250325

--- pruebaBaseDatos/src/main/java/com/mycompany/pruebabasedatos/PruebaBaseDatos.java
+++ pruebaBaseDatos/src/main/java/com/mycompany/pruebabasedatos/PruebaBaseDatos.java
@@ -18,16 +18,18 @@ public class PruebaBaseDatos {
         Connection con;
         con=DriverManager.getConnection("jdbc:mysql://localhost/nba","root","");        
         Statement sentencia=con.createStatement();
+        ResultSet rs;
+        String nombre="",consulta="";
         
-        System.out.println("Para consultar datos de un jugador introduce 1.\nPara añadir un jugador a la base de datos pulsa 2.\nPara eliminar un jugador pulsa 3.");
+        System.out.println("Para consultar datos de un jugador introduce 1.\nPara añadir un jugador a la base de datos pulsa 2.\nPara actualizar el equipo de un jugador pulsa 3.\nPara eliminar un jugador pulsa 4.");
         int opcion=sc.nextInt();
         
         switch(opcion){
             case 1:
                 System.out.println("Introduce el nombre");
-                String nombre=teclado.nextLine();
+                nombre=teclado.nextLine();
 
-                ResultSet rs=sentencia.executeQuery("select * from jugadores where Nombre='"+nombre+"';");
+                rs=sentencia.executeQuery("select * from jugadores where Nombre='"+nombre+"';");
 
                 int numColumnas = rs.getMetaData().getColumnCount();
 
@@ -43,11 +45,71 @@ public class PruebaBaseDatos {
                 System.out.println(); 
                 }
                 rs.close();
-            break;
+                break;
             
             case 2:
-                System.out.println("Introduce la siguiente informacion a guardar sobre el jugador:\ncodigo\nNombre\nProcedencia\nAltura\nPeso\nPosicion\nNombre_equipo");
+                String cadena="";
+                System.out.println("Introduce la siguiente informacion a guardar sobre el jugador: ");
+                System.out.println("codigo: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Nombre: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Procedencia: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Altura: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Peso: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Posicion: ");
+                cadena+="'"+teclado.nextLine()+"',";
+                System.out.println("Nombre de equipo: ");
+                cadena+="'"+teclado.nextLine()+"'";
                 
+                consulta="insert into jugadores values("+cadena+");";
+                try{
+                    int op=sentencia.executeUpdate(consulta);
+                    if(op==1){
+                        System.out.println("Jugador introducido.");
+                    }
+                }
+                catch(SQLException e){
+                    System.out.println(e.getMessage());
+                }                                                                                                                        
+                break;
+                
+            case 3:
+                System.out.println("Introduce el nombre del Jugador que ha cambiado de equipo: ");
+                nombre=teclado.nextLine();
+                System.out.println("Introduce el nuevo equipo");
+                String equipo=teclado.nextLine();
+                
+                consulta="update jugadores set Nombre_equipo='"+equipo+"' where Nombre='"+nombre+"';";
+                try{
+                    int op3=sentencia.executeUpdate(consulta);
+                    if(op3==1){
+                        System.out.println("Jugador actualizado");
+                    }
+                }
+                catch(SQLException ex){
+                    System.out.println(ex.getMessage());
+                }
+                break;
+            
+            case 4:
+                System.out.println("Introduce el nombre del jugador a eliminar: ");
+                nombre=teclado.nextLine();
+                
+                consulta="delete from jugadores where Nombre='"+nombre+"';";
+                try{
+                    int op4=sentencia.executeUpdate(consulta);
+                    if(op4==1){
+                        System.out.println("Jugador eliminado.");
+                    }
+                }
+                catch(SQLException exe){
+                    System.out.println(exe.getMessage());
+                }
+                break;
         }
         
         
